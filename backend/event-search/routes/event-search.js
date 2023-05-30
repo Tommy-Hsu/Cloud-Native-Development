1f9a48dbe50539468e72516f35c7e3ceb35365ca
unknown <shenkh.cs11@nycu.edu.tw>
2023-05-30 14:32:04 +0800

    Added /all-events and /event-search

---
+++ backend/event-search/routes/event-search.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+let Activity = require('../../db-endpoints-server/models/activity.model');
+
+router.route('/all-events').get((req, res) => {
+    Activity.find()
+    .then(activity => res.json(activity))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/event-search').get((req, res) => {
+    // Schema
+    // activityType: {type: String, required: true},
+    // name: {type: String, required: true},
+    // description: {type: String, required: true},
+    // price: {type: Number, required: true},
+    // minMember: {type: Number, required: true},
+    // currentMember: {type: Number, required: true},
+    // date: {type: Date, required: true},
+
+    const activityType=req.query.activityType;
+    const name=req.query.name;
+
+    if (activityType == undefined) { activityType = "*"; }
+    if (name == undefined) { name = "*"; }
+
+    console.log(activityType);
+    console.log(name);
+
+    Activity.find({name: new RegExp('^'+name+'$', "i"), activityType: activityType})
+    .then(activities => res.json(activities))
+    .catch(err => res.status(400).json('Error: ' + err));
+})
+
+module.exports = router;

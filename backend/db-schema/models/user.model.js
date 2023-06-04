2c91ff84ebd9e5997c153e8ce7a4d2e5e069a04a
Michael_Desktop <minglun.c.li@gmail.com>
2023-06-04 22:44:25 +0800

    new schema

--- backend/db-schema/models/user.model.js
+++ backend/db-schema/models/user.model.js
@@ -2,15 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minlength: 3,
-    },
-}, {
-    timestamps: true,
+    email: {type: String, required: true},
+    password: {type: String, required: true},
 });
 
 const User = mongoose.model('User', userSchema);
--- backend/db-schema/test-schema.js
+++ backend/db-schema/test-schema.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-let Activity = require('./models/activity.model');
 let User = require('./models/user.model');
 let Session = require('./models/session.model');
 let Group = require('./models/group.model');
@@ -14,46 +13,22 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 
     insertUser();
-    insertActivity();
     insertSession();
     insertGroup();
 })
 
 
 function insertUser() {
-    const username = "Test-username";
+    const email = "Test-email";
+    const password = "Test-password"
+
     const newUser = new User({
-        username,
+        email,
+        password,
     });
 
     newUser.save()
     .then(() => console.log('User added!'))
-    .catch(err => console.log('Error: ' + err));
-}
-
-function insertActivity() {
-    const name = "Test-name";
-    const type = Number(0);
-    const category = Number(0);
-    const description = "Test-description";
-    const price = Number(0);
-    const minMember = Number(0);
-    const currentMember = Number(0);
-    const date = Date();
-
-    const newActivity = new Activity({
-        name,
-        type,
-        category,
-        description,
-        price,
-        minMember,
-        currentMember,
-        date,
-    });
-
-    newActivity.save()
-    .then(() => console.log('Activity added!'))
     .catch(err => console.log('Error: ' + err));
 }
 

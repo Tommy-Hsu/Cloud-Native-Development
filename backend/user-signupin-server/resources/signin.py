c4a446edd9f244a15c645bf1cc98e43a020a6b7a
Tommy-Hsu <hpc880214@gmail.com>
2023-06-05 21:03:44 +0800

    Update signin.py

--- backend/user-signupin-server/resources/signin.py
+++ backend/user-signupin-server/resources/signin.py
@@ -34,7 +34,7 @@ class SigninUser(Resource):
             if mycol.find_one(mydict): # 檢查是否有重複的 email
                 uid = mycol.find_one(mydict)['_id']
                 database.sessions.insert_one({"uid": uid, "session": str(uid)})
-                response = make_response(jsonify({'msg': 0}))
+                response = make_response(jsonify({'msg': 0, "session": str(uid)}))
                 response.status_code = 200
                 return response
             else:

ca7f7813e3907eec4a2b3500f9238b1a65a2659c
Michael_Desktop <minglun.c.li@gmail.com>
2023-06-06 06:38:36 +0800

    remove env port of server.js

--- backend/db-endpoints-server/server.js
+++ backend/db-endpoints-server/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());

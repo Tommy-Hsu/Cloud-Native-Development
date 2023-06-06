71b9cd205616c291b4a5f26bcf62b0e4e7b408c9
WalterZhang37 <zhangbohan.mg10@nycu.edu.tw>
2023-06-07 04:13:31 +0800

    frontend_update

--- frontend/src/com/Navbar.js
+++ frontend/src/com/Navbar.js
@@ -52,23 +52,22 @@ const CustomHeader = () => {
   const handleButtonClicked = () => {
     // 在这里执行其他操作或导航到其他页面
   };
-
   return (
-    <Header style={{ background: '#fff' }}>
+    <Header style={{ background: '#fff', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)' }}>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
         <div style={{ display: 'flex', alignItems: 'center', marginLeft: '15%' }}>
-          <Link to="/" style={{ marginRight: '16px', display: 'flex', alignItems: 'center' }}>
+          <Link to="/" style={{ marginRight: '16px', display: 'flex', alignItems: 'center', color: '#1890ff' }}>
             <img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="Logo" style={{ width: 'auto', height: '30px' }} />
-            <span style={{ marginRight: '16px' }}>Chilltan</span>
+            <span style={{ marginRight: '16px', fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '1.2em' }}>Chilltan</span>
           </Link>
-          
           <span style={{ marginRight: '16px' }}>
-            <Button type="primary" onClick={handleButtonClicked}>
-            <Link to="/create">提案!</Link>
-          </Button>
-          </span><Button type="primary" onClick={handleButtonClicked}>
-              <Link to="/search">搜尋</Link>
+            <Button type="primary" style={{ borderRadius: '5px' }} onClick={handleButtonClicked}>
+              <Link to="/create" style={{ color: '#fff' }}>提案!</Link>
             </Button>
+          </span>
+          <Button type="primary" style={{ borderRadius: '5px' }} onClick={handleButtonClicked}>
+            <Link to="/search" style={{ color: '#fff' }}>搜尋</Link>
+          </Button>
         </div>
         
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', marginRight: '15%' }}>
@@ -78,7 +77,7 @@ const CustomHeader = () => {
                 <span style={{ cursor: 'pointer' }}>
                   <Space>
                     <Avatar icon={userData.userIcon} />
-                    {userData.hoverText}
+                    <span style={{ fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '1.1em' }}>{userData.hoverText}</span>
                   </Space>
                 </span>
               </Dropdown>
@@ -87,11 +86,11 @@ const CustomHeader = () => {
             <>
               <span style={{ marginRight: '16px' }}>
                 <Link to="/login">
-                  <Button>登入</Button>
+                  <Button style={{ borderRadius: '5px' }}>登入</Button>
                 </Link>
               </span>
               <Link to="/register">
-                <Button>注冊</Button>
+                <Button style={{ borderRadius: '5px' }}>注冊</Button>
               </Link>
             </>
           )}
@@ -102,3 +101,52 @@ const CustomHeader = () => {
 };
 
 export default CustomHeader;
+//   return (
+//     <Header style={{ background: '#fff' }}>
+//       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+//         <div style={{ display: 'flex', alignItems: 'center', marginLeft: '15%' }}>
+//           <Link to="/" style={{ marginRight: '16px', display: 'flex', alignItems: 'center' }}>
+//             <img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="Logo" style={{ width: 'auto', height: '30px' }} />
+//             <span style={{ marginRight: '16px' }}>Chilltan</span>
+//           </Link>
+          
+//           <span style={{ marginRight: '16px' }}>
+//             <Button type="primary" onClick={handleButtonClicked}>
+//             <Link to="/create">提案!</Link>
+//           </Button>
+//           </span><Button type="primary" onClick={handleButtonClicked}>
+//               <Link to="/search">搜尋</Link>
+//             </Button>
+//         </div>
+        
+//         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', marginRight: '15%' }}>
+//           {loggedIn ? (
+//             <span style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', marginRight: '43px' }}>
+//               <Dropdown trigger={['click']} dropdownRender={() => menu}>
+//                 <span style={{ cursor: 'pointer' }}>
+//                   <Space>
+//                     <Avatar icon={userData.userIcon} />
+//                     {userData.hoverText}
+//                   </Space>
+//                 </span>
+//               </Dropdown>
+//             </span>
+//           ) : (
+//             <>
+//               <span style={{ marginRight: '16px' }}>
+//                 <Link to="/login">
+//                   <Button>登入</Button>
+//                 </Link>
+//               </span>
+//               <Link to="/register">
+//                 <Button>注冊</Button>
+//               </Link>
+//             </>
+//           )}
+//         </div>
+//       </div>
+//     </Header>
+//   );
+// };
+
+// export default CustomHeader;

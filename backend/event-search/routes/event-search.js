d2680c78c4089b2dcc713bc0a3e844cd53b4b6ab
unknown <shenkh.cs11@nycu.edu.tw>
2023-06-06 21:43:55 +0800

    Update event-search.js

--- backend/event-search/routes/event-search.js
+++ backend/event-search/routes/event-search.js
@@ -17,18 +17,19 @@ router.route('/event-search').get((req, res) => {
     // currentMember: {type: Number, required: true},
     // date: {type: Date, required: true},
 
-    var type=req.query.type;
-    var category=req.query.category;
+    var type=[req.query.type];
+    var category=[req.query.category];
     var title=req.query.title;
 
-    if (type == undefined) { type = ""; }
-    if (category == undefined) { category = ""; }
+    if (type[0] == undefined) { type = [0, 1]; }
+    if (category[0] == undefined) { category = [0, 1, 2, 3, 4, 5]; }
     if (title == undefined) { title = ""; }
 
-    console.log(type);
     console.log(title);
+    console.log(type);
+    console.log(category)
 
-    Group.find({title: {$regex: title, $options: 'i'}, type: {$regex: type}})
+    Group.find({title: {$regex: title, $options: 'i'}, type: {$in: type}, category: {$in: category}})
     .then(groups => res.json(groups))
     .catch(err => res.status(400).json('Error: ' + err));
 })
